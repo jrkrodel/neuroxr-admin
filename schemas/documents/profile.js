9da9a2366e029b4b294e5174f19a4ce5629828d2
jrkrodel <78804328+jrkrodel@users.noreply.github.com>
2022-12-01 19:24:30 -0500

    missed one casing

--- schemas/documents/profile.js
+++ schemas/documents/profile.js
@@ -24,7 +24,7 @@ export default {
               {
                 name: "link",
                 type: "object",
-                title: "link",
+                title: "Link",
                 fields: [
                   {
                     name: "url",
